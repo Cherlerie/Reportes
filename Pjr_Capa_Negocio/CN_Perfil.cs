0df7ec8b1d24467eebb9d57f06838dcb1412ef83
Cherlerie <genesismmejia3@gmail.com>
2025-01-29 10:11:48 -0400

    cscad4

--- Pjr_Capa_Negocio/CN_Perfil.cs
+++ Pjr_Capa_Negocio/CN_Perfil.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Pjr_Capa_Datos;
+using Pjr_Capa_Entidad;
 
 namespace Pjr_Capa_Negocio
 {
     public class CN_Perfil
     {
+        public CD_Perfil cdPerfil = new CD_Perfil();
+
+        public bool EditarPerfil(CE_perfil perfil)
+        {
+            return cdPerfil.EditarPerfil(perfil.ClienteID1, perfil.Nombre1, perfil.Apellido1, perfil.Contraseña1, perfil.Correo, perfil.Direccion1, perfil.Telefono1);
+        }
+
+        public CE_perfil ObtenerPerfil(int id)
+        {
+            return cdPerfil.ObtenerPerfil(id);
+        }
     }
 }
