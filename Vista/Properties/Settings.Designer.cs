aac848e4014c00d0dba42bf4a02cb336e95d6baa
Hasley Forch <ashmarielforch01@gmail.com>
2025-02-07 10:53:09 -0400

    cambios realizados

--- Vista/Properties/Settings.Designer.cs
+++ Vista/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Vista.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace Vista.Properties {
         public string cn {
             get {
                 return ((string)(this["cn"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-3JASJH5\\MSSQLSERVER01;Initial Catalog=Prestamosdatabase;Integ" +
+            "rated Security=True;TrustServerCertificate=True")]
+        public string PrestamosdatabaseConnectionString {
+            get {
+                return ((string)(this["PrestamosdatabaseConnectionString"]));
             }
         }
     }
